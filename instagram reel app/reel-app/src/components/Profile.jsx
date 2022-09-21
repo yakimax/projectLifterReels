de36d8178c0bf1df33e34661f61550d192071a99
yakimax <sv1099912@gmail.com>
2022-09-22 00:36:59 +0530

    add

--- instagram reel app/reel-app/src/components/Profile.jsx
+++ instagram reel app/reel-app/src/components/Profile.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import '../App.css'
 
 function Profile() {
-  return 
-  (
+  return (
     <>
       <div className='header'></div>
         <div className="main">
-          <div className="profileImage"></div>
+          <div className="img_container">
+            <img src = "https://picsum.photos/200" className='pimg' alt=""></img>
+          </div>
           <div className="details">
             <div className="content">Name :</div>
             <div className="content">No of posts :</div>
