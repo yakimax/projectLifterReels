cb1cd8fee775824817b47023d0b963b883b7f93b
yakimax <sv1099912@gmail.com>
2022-09-24 00:35:20 +0530

    add

--- instagram reel app/reel-app/src/components/Feed.jsx
+++ instagram reel app/reel-app/src/components/Feed.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../App.css';
-import {FontAwesomeIcon} from ''
 
 function Feed() {
   return (
@@ -10,7 +9,7 @@ function Feed() {
       <img className='profile_img' src = "https://image.shutterstock.com/image-vector/man-icon-vector-260nw-1040084344.jpg" alt = 'ProfileImg'/>
     </div>
     <div className='main_container'>
-      <div className='upload_container'><FontAwesomeIcon icon="fa-solid fa-clapperboard" /><button>Upload video</button></div>
+      <div className='upload_container'><i class="fa-solid fa-clapperboard"></i></div>
       <div className='reels_container'></div>
     </div>
     </>
