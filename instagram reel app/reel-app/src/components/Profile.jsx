67f6c36eaff7ccb46d5c49e862756cfd4f85f0a7
yakimax <sv1099912@gmail.com>
2022-09-25 12:14:41 +0530

    add

--- instagram reel app/reel-app/src/components/Profile.jsx
+++ instagram reel app/reel-app/src/components/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react'
+import { useEffect } from 'react';
 import '../App.css'
 import { AuthContext } from '../context/AuthContext'
 
@@ -6,24 +7,28 @@ function Profile() {
   let userObj = useContext(AuthContext);
   const [loader ,setLoader] = useState(false);
   
-  
+  useEffect(){
+    
+  }
   return (
     <>
     {
     userObj.currUser == null ? <div>getting data </div>:
-    <div>{userObj.currUser.uid}</div>
-      /* <div className='header'></div>
-        <div className="main">
-          <div className="img_container">
-            <img src = "https://picsum.photos/200" className='pimg' alt=""></img>
-          </div>
-          <div className="details">
-            <div className="content">Name :{userObj.user.uid}</div>
-            <div className="content">No of posts :</div>
-            <div className="content">Mail me :</div>
-          </div>
-      </div> */}
-
+    <>
+      <div>{userObj.currUser.uid}</div>
+        <div className='header'></div>
+            <div className="main">
+              <div className="img_container">
+              <img src = "https://picsum.photos/200" className='pimg' alt=""></img>
+            </div>
+            <div className="details">
+              <div className="content">Name :{userObj.user.uid}</div>
+              <div className="content">No of posts :</div>
+              <div className="content">Mail me :</div>
+            </div>
+      </div> 
+    </>
+    }
     </>
   )
 }
